76c87f2e63467abf9a7400211668c08b6fa46e5e
NadzeyaValatkevich <nadkonadzya@rambler.ru>
2024-04-09 20:26:35 +0300

    fix url

--- src/redux/api/index.ts
+++ src/redux/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://${API_HOST}:8000/api',
+    baseURL: `http://${API_HOST}:8000/api`,
 });
 
 export const objectsApi = {
