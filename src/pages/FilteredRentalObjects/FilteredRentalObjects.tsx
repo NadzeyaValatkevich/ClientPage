57936d4bdde34c7630ce62ea903bc43bd51208d1
NadzeyaValatkevich <nadkonadzya@rambler.ru>
2024-04-29 16:03:58 +0300

    add the functionality of saving the state on page reload

--- src/pages/FilteredRentalObjects/FilteredRentalObjects.tsx
+++ src/pages/FilteredRentalObjects/FilteredRentalObjects.tsx
@@ -1,20 +1,40 @@
 import style from "./FilteredRentalObjects.module.scss";
 import styleContainer from "../../common/styles/Container.module.scss";
-import { useAppSelector } from "../../utils/hooks";
+import { useAppDispatch, useAppSelector } from "../../utils/hooks";
 import { CommonHouseCard } from "../../components/CommonHouseCard/CommonHouseCard";
 import { RentalObject } from "../../redux/types/rentalObjectTypes";
 import { countRooms } from "../../utils/functions/countRooms";
 import { features } from "../Main/Main";
 import { Button } from "../../components/Button/Button";
 import { FullHouseCard } from "../../components/FullHouseCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Modal } from "../../components/Modal";
 import { Booking } from "../../components/Booking";
+import { useSearchParams } from "react-router-dom";
+import { DatesGuestsObjectRequestType } from "../../redux/types/datesGuestsTypes";
+import { fetchFilteredRentalObjects } from "../../redux/thunks/filteredRentalObjectThunk";
 
 export const FilteredRentalObjects = () => {
     const { results, count } = useAppSelector(state => state.filteredRentalObjects);
     const [modalActive, setModalActive] = useState(false);
     const [modalBookingActive, setModalBookingActive] = useState(false);
+
+    const [searchParams] = useSearchParams();
+
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        const queryParams = new URLSearchParams(searchParams);
+
+        const queryParamsData: DatesGuestsObjectRequestType = {
+            check_in_date: queryParams.get('check_in_date'),
+            check_out_date: queryParams.get('check_out_date'),
+            people_amount: queryParams.get('people_amount'),
+            main_object: queryParams.get('main_object'),
+        };
+
+        dispatch(fetchFilteredRentalObjects(queryParamsData))
+    }, [searchParams, dispatch])
 
     const onClickHandler = () => {
         setModalActive(true)
--- src/redux/types/datesGuestsTypes.ts
+++ src/redux/types/datesGuestsTypes.ts
@@ -1,23 +1,23 @@
 export type ChildAge = {
-    value: number,
-    label: string
-};
+  value: number
+  label: string
+}
 
 export type GuestsType = {
-    adults: number | null,
-    children: number | null,
-    childAges: Array<ChildAge | undefined>
-};
+  adults: number | null
+  children: number | null
+  childAges: Array<ChildAge | undefined>
+}
 
 export type DatesGuestsObjectType = {
-    check_in_date: Date | string,
-    check_out_date: Date | string,
-    guests: GuestsType
+  check_in_date: Date | string
+  check_out_date: Date | string
+  guests: GuestsType
 }
 
 export type DatesGuestsObjectRequestType = {
-    check_in_date: Date | string,
-    check_out_date: Date | string,
-    people_amount: number,
-    main_object: number 
+  check_in_date: Date | string | null
+  check_out_date: Date | string | null
+  people_amount: number | string | null
+  main_object: number | string | null
 }
