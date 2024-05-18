6dad4b897a10536d0e57c9f145b39b6d4a35fcc7
NadzeyaValatkevich <nadkonadzya@rambler.ru>
2024-05-18 14:39:26 +0300

    change API_HOST

--- src/redux/api/index.ts
+++ src/redux/api/index.ts
@@ -1,21 +1,23 @@
-import axios from "axios";
-import { API_HOST } from "../../utils/config";
-import { DatesGuestsObjectRequestType } from "../types/datesGuestsTypes";
+import axios from 'axios'
+import { API_HOST } from '../../utils/config'
+import { DatesGuestsObjectRequestType } from '../types/datesGuestsTypes'
 
 const instance = axios.create({
-    baseURL: `http://${API_HOST}:8000/api`,
-});
+  baseURL: `${API_HOST}/api`,
+})
 
 export const objectsApi = {
-    getMainObject(id: number) {
-        return instance.get(`/client_part/main_objects/${id}/`)
-    },
+  getMainObject(id: number) {
+    return instance.get(`/client_part/main_objects/${id}/`)
+  },
 
-    getRentalObjects() {
-        return instance.get(`/client_part/rental_objects/`)
-    },
+  getRentalObjects() {
+    return instance.get(`/client_part/rental_objects/`)
+  },
 
-    getFilteredRentalObjects(data: DatesGuestsObjectRequestType) {
-        return instance.get(`/client_part/rental_objects/prices?main_object=${data.main_object}&people_amount=${data.people_amount}&check_in_date=${data.check_in_date}&check_out_date=${data.check_out_date}`)
-    }
-};
+  getFilteredRentalObjects(data: DatesGuestsObjectRequestType) {
+    return instance.get(
+      `/client_part/rental_objects/prices?main_object=${data.main_object}&people_amount=${data.people_amount}&check_in_date=${data.check_in_date}&check_out_date=${data.check_out_date}`,
+    )
+  },
+}
