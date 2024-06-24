7d7d881be6fc11f62f4c0117f69f890b46d7683f
NadzeyaValatkevich <nadkonadzya@rambler.ru>
2024-06-25 02:43:14 +0300

    add adaptive to FilteredBlock

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -29,7 +29,7 @@ export const Layout = ({ scrollToFilteredObjects }: any) => {
             <Header />
             <FilteredBlock scrollToFilteredObjects={scrollToFilteredObjects} />
             <Outlet />
-            <Footer />
+            {/* <Footer /> */}
         </>
     )
 }
